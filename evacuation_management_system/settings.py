82a2d1107e03a8a123a732c0ed71b2f09636f9dd
Shaun Niel Ochavo <shaunniel02@gmail.com>
2024-05-19 15:38:27 +0800

    Modify the allowed hosts in settings.py

--- evacuation_management_system/settings.py
+++ evacuation_management_system/settings.py
@@ -9,8 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1',
-                       cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'backend.CustomUser'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
