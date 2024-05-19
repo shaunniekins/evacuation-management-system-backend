9b5e1688daf59ec4fec705b275d98a68a0ad54a8
Shaun Niel Ochavo <shaunniel02@gmail.com>
2024-05-19 19:06:41 +0800

    Add port for start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ python manage.py
 python manage.py makemigrations backend
 python manage.py migrate
 python create_superuser.py
-gunicorn evacuation_management_system.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn evacuation_management_system.wsgi:application --bind 0.0.0.0:8000
