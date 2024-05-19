24c7c0bdc632813c1c496bca148fc614a9cdcbb8
Shaun Niel Ochavo <shaunniel02@gmail.com>
2024-05-19 16:33:35 +0800

    Modify create_superuser from using os.environ.get to config

--- create_superuser.py
+++ create_superuser.py
@@ -1,18 +1,20 @@
 import os
 from django.contrib.auth import get_user_model
 from django.core.wsgi import get_wsgi_application
+from decouple import config
+
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'evacuation_management_system.settings'
 application = get_wsgi_application()
 
 User = get_user_model()
 
-if os.environ.get('CREATE_SUPERUSER'):
-    if not User.objects.filter(username=os.environ.get('DJANGO_SUPERUSER_USERNAME')).exists():
+if config('CREATE_SUPERUSER'):
+    if not User.objects.filter(username=config('DJANGO_SUPERUSER_USERNAME')).exists():
         User.objects.create_superuser(
-            username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
-            email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
-            password=os.environ.get('DJANGO_SUPERUSER_PASSWORD'),
+            username=config('DJANGO_SUPERUSER_USERNAME'),
+            email=config('DJANGO_SUPERUSER_EMAIL'),
+            password=config('DJANGO_SUPERUSER_PASSWORD'),
         )
     else:
         print("Superuser already exists. Not created.")
