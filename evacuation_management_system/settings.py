0304e15e36cfcafa0faaa50e3b7bc712673f7900
Shaun Niel Ochavo <shaunniel02@gmail.com>
2024-05-19 19:57:11 +0800

    Add postgres settings from render

--- evacuation_management_system/settings.py
+++ evacuation_management_system/settings.py
@@ -76,12 +76,16 @@ WSGI_APPLICATION = 'evacuation_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST', default='localhost'),
+        'PORT': config('POSTGRES_PORT', default='5432')
     }
 }
 
-DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
+# DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
 
 AUTH_PASSWORD_VALIDATORS = [
     {
