909b2cf2f8a1a72158f35483cd53c4e9486a5229
Shaun Niel Ochavo <shaunniel02@gmail.com>
2024-05-19 16:14:36 +0800

    Modify create_superuser to handle existing username and add password

--- create_superuser.py
+++ create_superuser.py
@@ -1,12 +1,18 @@
 import os
-from django.core.management import call_command
+from django.contrib.auth import get_user_model
 from django.core.wsgi import get_wsgi_application
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'evacuation_management_system.settings'
 application = get_wsgi_application()
 
+User = get_user_model()
+
 if os.environ.get('CREATE_SUPERUSER'):
-    call_command('createsuperuser',
-                 username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
-                 email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
-                 interactive=False)
+    if not User.objects.filter(username=os.environ.get('DJANGO_SUPERUSER_USERNAME')).exists():
+        User.objects.create_superuser(
+            username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
+            email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+            password=os.environ.get('DJANGO_SUPERUSER_PASSWORD'),
+        )
+    else:
+        print("Superuser already exists. Not created.")
